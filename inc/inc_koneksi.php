c63adfea57821c76e741b537d3c44d27c8580aa4
nabielubad <moh.rifdanabilubadah@gmail.com>
2023-10-30 21:54:33 +0700

    koneksi

--- inc/inc_koneksi.php
+++ inc/inc_koneksi.php
@@ -1,9 +1,14 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pass = "";
-$db = "informatika";
+$user = "redj3298_nabil";
+$pass = "nabil_farah";
+$db = "redj3298_informatikates";
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 if(!$koneksi){
     die("gagal terkoneksi bro");
 }
+
+//$host = "localhost";
+//$user = "redj3298_nabil";
+//$pass = "nabil_farah";
+//$db = "redj3298_informatikates";
